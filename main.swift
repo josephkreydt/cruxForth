e1af16789f24c14f7d4bafc41e6f5aac517846a0
Joseph <8309575+josephkreydt@users.noreply.github.com>
2021-05-26 18:52:59 -0400

    As of end of fourth video

--- main.swift
+++ main.swift
@@ -3,6 +3,8 @@ import Foundation
 print("Welcome to cruxForth\n>>>>", terminator:"")
 
 var intStack: [Int] = []
+var compileFlag: Bool = false
+var compiledWords: String = ""
 
 while let input = readLine() {
 	let input_array = input.components(separatedBy: " ")
@@ -17,24 +19,43 @@ while let input = readLine() {
 
 func processInput(input_array: [String]) -> Bool {
 	for input in input_array {
-		if Int(input) != nil {
-			intStack.append(Int(input) ?? 0)
+		
+		if compileFlag == true {
+			if input == "compiler" {
+				print(compiledWords)
+			} else if input == ";" {
+				endCompile()
+			} else {
+				compile(input: input)
+			}
 		} else {
-			//check dictionary
-			if input == "show" {
-				print(intStack)
-			} else if input == "bye" {
-				return false
-			} else if input == "+" {
-				add()
-			} else if input == "-" {
-				subtract()
-			} else if input == "*" {
-				multiply()
-			} else if input == "/" {
-				divide()
-			} else if input == "pop" {
-				pop()
+			if Int(input) != nil {
+				intStack.append(Int(input) ?? 0)
+			} else {
+				//check dictionary
+				if input == "show" {
+					print(intStack)
+				} else if input == "bye" {
+					return false
+				} else if input == "+" {
+					add()
+				} else if input == "-" {
+					subtract()
+				} else if input == "*" {
+					multiply()
+				} else if input == "/" {
+					divide()
+				} else if input == "pop" {
+					pop()
+				} else if input == ":" {
+					startCompile()
+				} else if input == "compiler" {
+					print(compiledWords)
+				} else if input == ";" {
+					print("not in compile mode")
+				} else if compiledWords.contains(":::: " + input) {
+					print("exists in dict")
+				}
 			}
 		}
 	}
@@ -76,4 +97,18 @@ func divide() {
 func pop() {
 	let topWord: Int = intStack.removeLast()
 	print(topWord)
+}
+
+func startCompile() {
+	compileFlag = true
+	compiledWords.append("::::")
+}
+
+func compile(input: String) {
+	compiledWords.append(" " + input)
+}
+
+func endCompile() {
+	compiledWords.append(" ;;;; ")
+	compileFlag = false
 }
